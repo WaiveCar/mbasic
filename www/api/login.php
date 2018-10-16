d52e02bdd636eba0a3d8857e50ca04c1809096ed
Chris McKenzie <kristopolous@yahoo.com>
2018-10-16 20:58:54 +0000

    propagation of bad password

--- www/api/login.php
+++ www/api/login.php
@@ -5,9 +5,13 @@ include ('common.php');
 $resJSON = post('/auth/login', $_POST);
 if(!array_key_exists('token', $resJSON)) {
   if(!empty($resJSON['message'])) {
-    $_SESSION['lasterror'] = $what['message'];
+    $_SESSION['lasterror'] = [
+      'title' => 'Unable to Log in',
+      'message' => $resJSON['message']
+    ];
   }
   header("Location: /index.php");
+  exit;
 }
 $_SESSION['token'] = $resJSON['token'];
 load('/showcars.php');

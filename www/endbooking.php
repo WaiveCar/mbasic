ac7f226abf2f9b3e2139030ddef9e9278e8e8539
Chris McKenzie <kristopolous@yahoo.com>
2018-10-20 14:22:43 -0700

    endbooking style cleanup

--- www/endbooking.php
+++ www/endbooking.php
@@ -16,24 +16,23 @@ doheader('End Booking');
     <form enctype="multipart/form-data" method="post" action="api/carcontrol.php?action=complete">
       <ol>
         <li>Take a photo of the parking sign<br/><small>If there is no parking sign, take a photo of the intersection to show that no sign is posted</small>
-          <p class='image-upload'>
+          <p class='image-upload ingroup'>
             <input name="parking" type="file" accept="image/*;capture=camera" required capture="camera" />
           </p>
         </li>
 
         <li>How long is the parking valid?<br/><small>Enter "48" if longer than 2 days</small>
-          <div>
-            <input class="input-inline" maxlength="3" autocomplete="off" type='number' name='hours' size='1' required > hours
+          <div class='ingroup'>
+            <input class="input-inline" maxlength="3" autocomplete="off" type='number' name='hours' size='1' required > Hours
           </div>
         </li>
 
         <li>Are there any new issues?<br><small>Add up to 4 photos</small>
 
-          <div class='image-upload'>
-            <input name="image0" type="file" accept="image/*;capture=camera" capture="camera" />
-            <input name="image1" type="file" accept="image/*;capture=camera" capture="camera" />
-            <input name="image2" type="file" accept="image/*;capture=camera" capture="camera" />
-            <input name="image3" type="file" accept="image/*;capture=camera" capture="camera" />
+          <div class='image-upload ingroup'>
+            <? for($ix = 0; $ix < 4; $ix++) { ?>
+            <input name="image<?= $ix ?>" type="file" accept="image/*;capture=camera" capture="camera" />
+            <? } ?>
           </div>
         </li>
 

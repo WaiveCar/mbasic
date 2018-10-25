dfb7317b15f27e889454a7f4133e4c1a4d82e2fc
Chris McKenzie <kristopolous@yahoo.com>
2018-10-25 14:02:42 -0700

    adding a revenue php

---
+++ www/revenue.php
@@ -0,0 +1,30 @@
+<?
+include('api/common.php');
+$revenueList = curldo('/revenue');
+$map = [
+  'name' => function($row) { return "<a href=//lb.waive.car/users/${row['user_id']}>{$row['first_name']} {$row['last_name']}</a>"; },
+  'status' => function($row) { return $row['status']; },
+  'revenue' => function($row) { return sprintf("%.2f", $row['ttl']/100); },
+  'tx count' => function($row) { return $row['charges']; },
+  'credit' => function($row) { return sprintf("%.2f", $row['credit']/100); }
+];
+
+?>
+<table>
+  <thead>
+    <? foreach(array_keys($map) as $column) { 
+      echo "<th>$column</th>";
+    } ?>
+  </thead>
+  <tbody>
+  <? foreach($revenueList as $row) {
+     echo "<tr>";
+     foreach($map as $key => $value) {
+       echo "<td>" . $value($row) . "</td>";
+     }
+     echo "</tr>";
+  }
+  ?>
+  </tbody>
+</table> 
+

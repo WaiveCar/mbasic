d9e87bd84268f5da482f3bb859ae81032a25c914
Chris McKenzie <kristopolous@yahoo.com>
2018-11-02 19:08:52 -0700

    style

--- www/inbooking.php
+++ www/inbooking.php
@@ -27,7 +27,7 @@ doheader('Current Booking', true, '<meta http-equiv="refresh" content="90">');
     <? showLocation($car); ?>
     <div align=center>
       <h4>You started <b><?= $timeStr ?>min</b> ago.</h4>
-      <a class=isolated href=control/end>Park and end ride</a>
+      <a class=isolated href=control/end>Park and End Ride</a>
     </div>
 
     <? actionList('control', [

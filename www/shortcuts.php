caf4e302dd80dc1f925d7a43551f1836e574f403
Chris McKenzie <kristopolous@yahoo.com>
2018-11-06 05:00:22 +0000

    updating links

--- www/shortcuts.php
+++ www/shortcuts.php
@@ -13,8 +13,8 @@ doheader('Your Account');
   <div class='content'>
     <p>With a few quick taps you can lock and unlock your WaiveCar right from your homescreen on your smartphone!</p>
     <p>Tap on each one and follow the instructions for each below and find out more!</p>
-    <a class='btn' href="control/unlock">Unlock</a>
-    <a class='btn' href="control/lock">Lock</a>
+    <a class='btn' href="Control/unlock">Unlock</a>
+    <a class='btn' href="Control/lock">Lock</a>
   </div>
 </body>
 </html>

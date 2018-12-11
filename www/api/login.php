bfd5b937731240c81cf6c8bd7f6a327a0822630a
Chris McKenzie <kristopolous@yahoo.com>
2018-12-10 18:09:31 -0800

    better response for login issues

--- www/api/login.php
+++ www/api/login.php
@@ -3,15 +3,22 @@
 include ('common.php');
 
 $resJSON = post('/auth/login', $_POST);
-if(!array_key_exists('token', $resJSON)) {
+if($resJSON === 'Not Found') {
+  $_SESSION['lasterror'] = [
+    'title' => 'Unable to Contact Server',
+    'message' => 'The server appears to be down. Please try again shortly'
+  ];
+} else if(!array_key_exists('token', $resJSON)) {
   if(!empty($resJSON['message'])) {
     $_SESSION['lasterror'] = [
       'title' => 'Unable to Log in',
       'message' => $resJSON['message']
     ];
   }
-  header("Location: /index.php");
+} else {
+  $_SESSION['token'] = $resJSON['token'];
+  load('/showcars.php');
   exit;
 }
-$_SESSION['token'] = $resJSON['token'];
-load('/showcars.php');
+header("Location: /index.php");
+exit;

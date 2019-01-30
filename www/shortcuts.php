777c365fc8270bd17799cbd5f843834bbc2ea157
Chris McKenzie <kristopolous@yahoo.com>
2019-01-29 19:21:19 -0800

    Typo fixes

--- www/shortcuts.php
+++ www/shortcuts.php
@@ -9,14 +9,14 @@ doheader('Your Account');
 ?>
 
 <div class='box prompt account'>
-  <h1>Add shortcuts to your homescreen</h1>
+  <h1>Add Shortcuts to your Homescreen</h1>
   <div class='content'>
     <p>With a few quick taps you can lock and unlock your WaiveCar right from your homescreen on your smartphone!</p>
     <center>
     <img style='border:1px solid black;padding:4px' src=/img/buttons.png>
     </center>
     
-    <p>Tap on each button below and follow the instructions to add it to your screen</p>
+    <p>Tap on each button below and follow the instructions to add it to your homescreen!</p>
     <p><a class='btn cta' href="Control/unlock">Unlock</a> shortcut</p>
     <p><a class='btn cta' href="Control/lock">Lock</a> shortcut</p>
   </div>

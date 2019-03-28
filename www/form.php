1b12e0d1e573e5dee10e77a15a0882f82cd13d9f
Chris McKenzie <kristopolous@yahoo.com>
2019-03-27 19:44:34 -0700

    Making the form control generic

--- www/form.php
+++ www/form.php
@@ -1,48 +1,36 @@
 <?
-include('api/common.php');
-list($path, $query) = explode('?', $_SERVER['HTTP_REFERER']);
-$title = aget($_GET, 't', 'Form');
-$help = aget($_GET, 'p', 'Please fill out the form');
-$back = $path;
-/*
-if(isset($_GET['b'])) {
-  $back = $_GET['b'];
-}
-if(isset($_GET['f'])) {
-  $path = $_GET['f'];
-}
- */
+include_once('api/common.php');
+
+$title = aget($_GET, 't|title', 'Form');
+$help = aget($_GET, 'h|help', 'Please fill out the form');
+$options = aget($_GET, 'o|options');
+$verb = aget($_GET, 'verb', 'get');
+$path = aget($_GET, 'a|action', $_SERVER['REQUEST_URI']);
+$back = aget($_GET, 'b|back');
+
 ob_start("sanitize_output");
-$title = "Enter Home Address";
-$help = "In an effort to improve service, please tell us your home address before continuing.";
-$options = [
-  ['name' => 'address1', 'label' => 'Address'],
-  ['name' => 'address2', 'label' => ''],
-  ['name' => 'city', 'size' => 50],
-  ['name' => 'state', 'size' => 20],
-  ['name' => 'zip', 'size' => '20']
-];
 
 ?>
 <!doctype html>
 <html>
 <head>
   <title><?= $title ?></title>
-  <meta name=viewport content="width=device-width, initial-scale=1.0" />
-  <link rel=stylesheet href=style.css>
+  <meta name=viewport content="width=device-width,initial-scale=1.0" />
+  <link rel=stylesheet href=/style.css>
 </head>
 <body>
   <div class='box prompt'>
     <h1><?= $title ?></h1>
     <div class=content>
       <div class=copy><?= $help ?></div>
-      <form method=get action="<?= $path ?>" class=prompt>
-        <? foreach($query as $key => $value) { ?>
-          <input name="<?=$key?>" type=hidden value="<?= $value ?>">
-        <? } ?>
+      <form method=<?= $verb ?> action="<?= $path ?>" class=prompt>
         <? 
         $ix = 0;
         foreach($options as $row) { 
+          if(aget($row, 'type') === 'hidden') {
+            echo "<input type=hidden name='${row['name']}' value='${row['value']}' />";
+            continue;
+          }
           $name = $row['name'];
           $label = aget($row, 'label', ucfirst($name));
           $size = aget($row, 'size', 100);
@@ -60,16 +48,22 @@ $options = [
           }
 
         ?>
-          <div class='input-container' <?= $style ?>>
+          <div class=input-container <?= $style ?>>
             <? if (strlen($label) > 0) { ?>
-              <label for="<?= $name ?>"><?= $label ?></label>
+              <label for=<?= $name ?>><?= $label ?></label>
             <? } ?>
-            <input class=input type=text name="<?= $name ?>" autofocus required autocomplete="off">
+            <input name=<?= $name ?> required>
           </div>
         <? } ?>
         <div class=action-list>
-          <a class='btn wid-2 ignore' href="<?= $back ?>">Cancel</a>
-          <input type='submit' value='Done' class='btn wid-2'>
+          <? 
+            $klass = '';
+            if (!empty($back)) { 
+              $klass = ' wid-2';
+              echo "<a class='btn wid-2 ignore' href='$back'>Cancel</a>";
+            } 
+          ?>
+          <input type=submit value=Done class='btn<?= $klass?>'>
         </div>
       </form>
     </div>

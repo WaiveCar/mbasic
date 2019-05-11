19a4640e96329b169ab955bbefe387a40f1d79d3
Chris McKenzie <kristopolous@yahoo.com>
2019-05-11 15:44:24 -0700

    expanding geo location cache lookup to allow multi lists

--- www/location.php
+++ www/location.php
@@ -1,3 +1,15 @@
 <?
 include('api/common.php');
-echo location($_GET);
+if(isset($_GET['latitutde'])) {
+  echo location($_GET);
+  // this format is multi=[[lat,lng],[lat,lng]...]
+} else if(isset($_GET['multi'])) {
+  $list = json_decode($_GET['multi'], true);
+  $res = [];
+  foreach($list as $row) {
+    $res[] = location([ 'latitude' => $row[0], 'longitude' => $row[1] ]);
+  }
+  echo json_encode($res);
+}
+
+

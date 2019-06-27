2c238844f680dddc58bad48d0642ceba442e4049
Chris McKenzie <kristopolous@yahoo.com>
2019-06-27 14:51:12 -0700

    supporting null locations

--- www/location.php
+++ www/location.php
@@ -7,7 +7,11 @@ if(isset($_GET['latitutde'])) {
   $list = json_decode($_GET['multi'], true);
   $res = [];
   foreach($list as $row) {
-    $res[] = location([ 'latitude' => $row[0], 'longitude' => $row[1] ]);
+    if(count($row) < 2 || empty($row[0]) || empty($row[1])) {
+      $res[] = null;
+    } else {
+      $res[] = location([ 'latitude' => $row[0], 'longitude' => $row[1] ]);
+    }
   }
   echo json_encode($res);
 }

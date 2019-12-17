bdbca9261657801d3811e1b099df0721cb0b27ef
Chris McKenzie <kristopolous@yahoo.com>
2019-12-17 14:14:38 -0800

    ws cleanup

--- www/inbooking.php
+++ www/inbooking.php
@@ -27,26 +27,24 @@ if(isWaiveWork()) {
 ob_start("sanitize_output");
 doheader('Current Booking', ['extraHtml' => '<meta http-equiv=refresh content=600>']);
 ?>
-  <div class=box>
-    <div class=tab-container>
-      <span>&#128663; <?= $car['license'] ?></span>
-      <a class=inactive href=fuel>&#128268; Chargers</a>
-    </div>
-    <? showLocation($car); ?>
-    <div align=center>
-      <h4><b><?= $timeStr ?></b> in <?= $car['license']; ?></h4>
-      <? if(!isWaiveWork()) { ?>
-      <a class=isolated href=end>Park and End Ride</a>
-      <? } ?>
-    </div>
-
-    <div class=content>
-    <? actionList(false, [
-      ['unlock', 'Unlock', 2],
-      ['lock', 'Lock', 2]
-    ]); ?>
-    </div>
+<div class=box>
+  <div class=tab-container>
+    <span>&#128663; <?= $car['license'] ?></span>
+    <a class=inactive href=fuel>&#128268; Chargers</a>
   </div>
-
+  <? showLocation($car); ?>
+  <div align=center>
+    <h4><b><?= $timeStr ?></b> in <?= $car['license']; ?></h4>
+    <? if(!isWaiveWork()) { ?>
+    <a class=isolated href=end>Park and End Ride</a>
+    <? } ?>
+  </div>
+  <div class=content>
+  <? actionList(false, [
+    ['unlock', 'Unlock', 2],
+    ['lock', 'Lock', 2]
+  ]); ?>
+  </div>
+</div>
 </body>
 </html>

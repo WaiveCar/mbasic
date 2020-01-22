1825ddadd95c5c4ed47e3e929c3ddfeca1395172
Chris McKenzie <kristopolous@yahoo.com>
2020-01-22 02:36:25 +0000

    basing location for charging off phone, not telematics

--- www/charge.js
+++ www/charge.js
@@ -1,12 +1,17 @@
 window.onload = function () {
   let geo = !window.location.search;
+  let gran = 600000;
   if(!geo) {
     let res = window.location.search.match(/at=(\d*)$/);
-    geo = new Date() - +( res ? res[1] : 0 ) > 600000;
+    geo = Math.floor(new Date()/gran) - +( res ? res[1] : 0 )
   }
   if(geo) {
     loc(function(pos){
-      window.location = window.location + "?lat=" + pos.coords.latitude + '&lng=' + pos.coords.longitude + "&at=" + +new Date();
+      window.location = window.location.toString().split('?')[0] + "?" + [ 
+        'latitude=' + pos.coords.latitude.toFixed(5), 
+        'longitude=' + pos.coords.longitude.toFixed(5), 
+        'at=' + Math.floor(new Date()/gran)
+      ].join('&');
     }, function () {
       console.log("nope");
     })

efae66a557ca8844bd925643591366447b68cb1b
Chris McKenzie <kristopolous@yahoo.com>
2020-01-21 18:04:05 -0800

    cutting out the zeros on the charger page

--- www/chargeList.php
+++ www/chargeList.php
@@ -74,11 +74,9 @@ if(count($chargerList) === 0) {
     <?
     foreach(['fast', 'slow'] as $type) {
       $len = $charger[$type];
-      $klass =$type;
-      if(!$len) {
-        $klass = "'$type zero'";
+      if($len) {
+        echo "<span class=$type>$len $type</span>";
       }
-      echo "<span class=$klass>$len $type</span>";
     }
     echo '</li>';
     $ix++;

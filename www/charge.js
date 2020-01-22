dc4b7526fb8b3681be64965d946a0535ceb0ae54
Chris McKenzie <kristopolous@yahoo.com>
2020-01-21 18:22:08 -0800

    pull new stuff every 10 minutes

--- www/charge.js
+++ www/charge.js
@@ -1,5 +1,10 @@
-window.onload = function (){
-  if(!window.location.search) {
+window.onload = function () {
+  let geo = !window.location.search;
+  if(!geo) {
+    let res = window.location.search.match(/at=(\d*)/);
+    geo = new Date() - +( res ? res[1] : 0 ) > 600000;
+  }
+  if(geo) {
     loc(function(pos){
       window.location = window.location + "?lat=" + pos.coords.latitude + '&lng=' + pos.coords.longitude + "&at=" + (new Date());
     }, function () {
